b05dc804781c57aa0612654835285f5c485dc56a
laomd-2 <laomd@mail2.sysu.edu.cn>
2018-10-10 16:11:07 +0800

    实验1

--- 1/1.1.sql
+++ 1/1.1.sql
@@ -22,45 +22,45 @@ CREATE TABLE IF NOT EXISTS Supplier (   --供应商基本表
     accbal REAL,                        --供应商账户余额
     comment VARCHAR(101)                --备注
 );
-CREATE TABLE IF NOT EXISTS Part (
-    partkey INTEGER PRIMARY KEY,
-    name VARCHAR(55),
-    mfgr CHAR(25),
-    brand CHAR(10),
-    type VARCHAR(25),
-    size INTEGER,
-    container CHAR(10),
-    retailprice REAL,
-    comment VARCHAR(23)
+CREATE TABLE IF NOT EXISTS Part (       --零件基本表
+    partkey INTEGER PRIMARY KEY,        --零件编号（主键）
+    name VARCHAR(55),                   --零件名称
+    mfgr CHAR(25),                      --制造厂
+    brand CHAR(10),                     --品牌
+    type VARCHAR(25),                   --零件类型
+    size INTEGER,                       --尺寸
+    container CHAR(10),                 --包装
+    retailprice REAL,                   --零售价格
+    comment VARCHAR(23)                 --备注
 );
-CREATE TABLE IF NOT EXISTS PartSupp (
-    partkey INTEGER REFERENCES Part (partkey),
-    suppkey INTEGER REFERENCES Supplier (suppkey),
-    availqty INTEGER,
-    supplycost REAL,
-    comment VARCHAR(199),
-    PRIMARY KEY (partkey , suppkey)
+CREATE TABLE IF NOT EXISTS PartSupp (   --零件供应联系表
+    partkey INTEGER REFERENCES Part (partkey),  --零件编号（外键）
+    suppkey INTEGER REFERENCES Supplier (suppkey),  --零件供应商编号（外键）
+    availqty INTEGER,                   --可用数量
+    supplycost REAL,                    --供应价格
+    comment VARCHAR(199),               --备注
+    PRIMARY KEY (partkey , suppkey)     --主键，表级约束
 );
-CREATE TABLE IF NOT EXISTS Customer (
-    custkey INTEGER PRIMARY KEY,
-    name VARCHAR(25),
-    address VARCHAR(40),
-    nationkey INTEGER REFERENCES Nation (nationkey),
-    phone CHAR(15),
-    acctbal REAL,
-    mktsegment CHAR(10),
-    comment VARCHAR(117)
+CREATE TABLE IF NOT EXISTS Customer (   --顾客表
+    custkey INTEGER PRIMARY KEY,        --顾客编号（主键）
+    name VARCHAR(25),                   --顾客名字
+    address VARCHAR(40),                --地址
+    nationkey INTEGER REFERENCES Nation (nationkey),    --国籍编号
+    phone CHAR(15),                     --电话
+    acctbal REAL,                       --账户余额
+    mktsegment CHAR(10),                --市场分区
+    comment VARCHAR(117)                --备注
 );
-CREATE TABLE IF NOT EXISTS Orders (
-    orderkey INTEGER PRIMARY KEY,
-    custkey INTEGER REFERENCES Customer (custkey),
-    orderstatus CHAR(1),
-    totalprice REAL,
-    orderdate DATE,
-    orderpriority CHAR(15),
-    clerk CHAR(15),
-    shippriority INTEGER,
-    comment VARCHAR(79)
+CREATE TABLE IF NOT EXISTS Orders (     --订单表
+    orderkey INTEGER PRIMARY KEY,       --订单编号（主键）
+    custkey INTEGER REFERENCES Customer (custkey),  --顾客编号（外键）
+    orderstatus CHAR(1),                --订单状态
+    totalprice REAL,                    --总金额
+    orderdate DATE,                     --日期
+    orderpriority CHAR(15),             --优先级
+    clerk CHAR(15),                     --记账员
+    shippriority INTEGER,               --运输优先级
+    comment VARCHAR(79)                 --备注
 );
 CREATE TABLE IF NOT EXISTS Lineitem (
     orderkey INTEGER REFERENCES Orders (orderkey),
