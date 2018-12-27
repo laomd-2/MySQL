6105ebc6b89fa3fba9b7d84493cc21a4d7abfaa7
laomd-2 <laomd@mail2.sysu.edu.cn>
2018-12-27 23:35:20 +0800

    数据库项目——在线商城

--- ecommerce/orders/views.py
+++ ecommerce/orders/views.py
@@ -1,24 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .models import Order
 
 
 def order_create(request):
     cart = Cart(request)
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
-        if form.is_valid():
-            order = form.save()
-            for item in cart:
-                OrderItem.objects.create(
-                    order=order,
-                    product=item['product'],
-                    price=item['price'],
-                    quantity=item['quantity']
-                )
-            cart.clear()
-            return render(request, 'orders/order/created.html', {'order': order})
+        try:
+            if form.is_valid():
+                order = form.save()
+                for item in cart:
+                    OrderItem.objects.create(
+                        order=order,
+                        product=item['product'],
+                        price=item['price'],
+                        quantity=item['quantity']
+                    )
+                cart.clear()
+                return render(request, 'orders/order/created.html', {'order': order})
+        except:
+            old_order = get_object_or_404(Order, id=order.id)
+            old_order.delete()
+            return HttpResponse('404 剩余数量不够')
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'form': form})
